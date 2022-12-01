e62ecac3e56e30901882c817cca82cda5f283f7f
Tathagat Vats <tcoder37@Tathagats-MacBook-Air.local>
2022-12-01 16:34:32 +0530

    bug fix

--- components/About.tsx
+++ components/About.tsx
@@ -9,7 +9,7 @@ export default function About({ }: Props) {
     return (
         <div className='flex flex-col relative text-center h-screen md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center'>
             {/*on a phone it would be a column arranged content while on the medium screen content will be row arranged*/}
-            <h3 className='absolute top-24 bottom-24 uppercase text-gray-500 text-2xl hover:text-cyan-400 '>
+            <h3 className='absolute top-24 bottom-24 uppercase text-gray-500 text-2xl tracking-wide hover:text-cyan-400 '>
                 About
             </h3>
 
@@ -29,10 +29,10 @@ export default function About({ }: Props) {
                 }}
                 src="https://i.postimg.cc/BnmkSBWD/Screenshot-2022-11-25-at-5-42-28-AM.png"
                 alt="my picture"
-                className='relative top-36 md:top-0 sm:mt-20 mb-20 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full object-cover md:w-64 md:h-54 md:rounded-lg border border-cyan-100'
+                className='relative top-28 md:top-0 sm:mt-20 mb-20 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full object-cover md:w-64 md:h-54 md:rounded-lg border border-cyan-100'
             />
             
-            <div className="space-y-10 px-0 md:px-10">
+            <div className="space-y-10 px-0 md:px-10 mt-7 md:mt-0">
                 <h4 className='text-2xl font-semibold md:text-4xl' >
                     Who <span  className='text-cyan-300'>am </span>I ?
                 </h4>
